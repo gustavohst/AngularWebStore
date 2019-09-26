00e668106af28189fde20b454a7d313411661c5d
Gustavo Tenório <gustavohst@msn.com>
2019-09-25 21:58:04 -0300

    Start typescript

---
+++ typescript/dist/app.js
@@ -0,0 +1,9 @@
+var message = "Help me, Obi-Wan Kenobi";
+console.log(message);
+var episode = 4;
+console.log("This is episode " + 4);
+episode = episode + 1;
+console.log("Next episode is " + episode);
+var favoriteDroid;
+favoriteDroid = 'BB-8';
+console.log("My favorite droid is " + favoriteDroid);
